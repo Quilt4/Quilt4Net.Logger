fb3e94af31c7039fc2f3268d203c41c42f1364d5
Daniel Bohlin <poxet@clowncrew.com>
2015-10-22 15:15:34 +0200

    Added the main project and tests

---
+++ Quilt4Net.Tests/SessionTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Tharga.Quilt4Net.Tests
+{
+    [TestFixture]
+    public class SessionTests
+    {
+        [Test]
+        public void x()
+        {
+            //Arrange
+            var session = new Session();
+
+            //Act
+            //var response = session.Register();
+
+            //Assert
+            //Assert.That(response, Is.Not.Null);
+        }
+    }
+}
---
+++ Quilt4Net/Session.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Tharga.Quilt4Net
+{
+    public class Session
+    {
+        //public void BeginRegister()
+        //{
+        //    //Starts an unmanaged task that queues the register message, to be executed later.
+        //    throw new NotImplementedException();
+        //}
+
+        public SessionResponse Register()
+        {
+            //Executes and waits for the result to come back from the server
+            throw new NotImplementedException();
+        }
+
+        //public SessionResponse Register(TimeSpan timeout)
+        //{
+        //    //Executes and waits for the result to come back from the server
+        //    throw new NotImplementedException();
+        //}
+
+        //public async Task<SessionResponse> RegisterAsync()
+        //{
+        //    //Executes the register and awaits the response from the server.
+        //    throw new NotImplementedException();
+        //}
+
+        //public async Task<SessionResponse> RegisterAsync(TimeSpan timeout)
+        //{
+        //    //Executes the register and awaits the response from the server.
+        //    throw new NotImplementedException();
+        //}
+    }
+}
