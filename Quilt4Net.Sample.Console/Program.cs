7585a127805c8689e0baa17e8686006412fb1765
Daniel Bohlin <daniel.bohlin@gmail.com>
2015-11-29 22:26:24 +0100

    Separated commands and queries

--- Quilt4Net.Sample.Console/Program.cs
+++ Quilt4Net.Sample.Console/Program.cs
@@ -22,8 +22,7 @@ namespace Quilt4Net.Sample.Console
                 var client = new Client(new WebApiClient(new Uri(string.Format("http://localhost:{0}/", port)), new TimeSpan(0, 0, 0, 30)));
                 //await client.User.CreateAsync("daniel.bohlin@gmail.com", "Qwerty!1");
                 await client.User.Login("daniel.bohlin@gmail.com", "Qwerty!1");
-
-                await client.Project.CreateAsync("Some project");
+                //await client.Project.CreateAsync("Some project");
 
                 var projects = await client.Project.GetAllAsync();
                 foreach (var project in projects)
